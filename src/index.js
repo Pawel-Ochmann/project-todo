b2a7c457f1416fbc0ddd0c0e0e2b559153a43651
Pawel Ochmann <pawelochmann@poczta.onet.pl>
2023-06-06 11:15:48 +0200

    Create and setup directory tree for project, add plugins, extensions etc, set configs files

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './scss/main.scss';
+
+const body = document.querySelector('body');
+const testDiv = document.createElement('div');
+testDiv.classList.add('test');
+body.appendChild(testDiv);
