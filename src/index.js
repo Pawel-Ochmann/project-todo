c9846dea474af679b7e592142bae36d6cb51ae0b
Pawel Ochmann <pawelochmann@poczta.onet.pl>
2023-06-09 14:51:59 +0200

    Create header, first attempt to style it, not satisfied tho, make it fixed positioned

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
 import './scss/main.scss';
 
-const body = document.querySelector('body');
-const testDiv = document.createElement('span');
-testDiv.classList.add('fa-solid', 'fa-plus');
-body.appendChild(testDiv);
+
