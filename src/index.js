7c940338b98ff1a2e0007fafcfad0165e63228f5
Pawel Ochmann <pawelochmann@poczta.onet.pl>
2023-06-09 17:19:01 +0200

    Create project section header, and add new project button, now working on form that allows user to input data for new project, dialog box displays properly

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 import './scss/main.scss';
 
+const body = document.querySelector('body');
+const header = document.querySelector('header');
+window.addEventListener('scroll', () => {
+  if (window.pageYOffset > 100) {
+    header.classList.add('sticky');
+  } else {
+    header.classList.remove('sticky');
+  }
+});
 
+function createProject() {
+  const addProjectForm = document.createElement('dialog');
+  addProjectForm.textContent = 'This is a project form!';
+  body.appendChild(addProjectForm);
+  addProjectForm.showModal();
+}
+const addProjectButton = document.querySelector('.projectHeader>button');
+addProjectButton.addEventListener('click', createProject);
