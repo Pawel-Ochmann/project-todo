e39db9a8b56ca02bb7f11f675133dacfb61d9ec7
Pawel Ochmann <pawelochmann@poczta.onet.pl>
2023-06-09 13:15:25 +0200

    Another pack of settings - get fonts, get main colors, shadows for it, create variables, add library with font awesome, test everything

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './scss/main.scss';
 
 const body = document.querySelector('body');
-const testDiv = document.createElement('div');
-testDiv.classList.add('test');
+const testDiv = document.createElement('span');
+testDiv.classList.add('fa-solid', 'fa-plus');
 body.appendChild(testDiv);
